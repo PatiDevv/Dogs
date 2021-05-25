4cd2195a415ab275e94cfd9f53ec597f918266c7
PatiDev95 <54117153+PatiDev95@users.noreply.github.com>
2021-05-25 13:18:12 +0200

    Conditional rendering of form fields.

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -11,7 +11,7 @@ interface Props {
 }
 
 const types = {
-    twitter: "twitter",
+    facebook: "facebook",
     article: "article",
     note: "note"
 }
@@ -20,7 +20,7 @@ export type Types = keyof typeof types
 
 
 const descriptions = {
-    twitter: "nową grupę psów rasowych na fb",
+    facebook: "nową grupę psów rasowych na fb",
     article: "artykuł o psach rasowych",
     note: "swoją notatkę odnośnie rasy"
 }
@@ -28,7 +28,7 @@ const descriptions = {
 class Form extends React.Component<Props> {
 
     state = {
-        activeOption: types.twitter as Types,
+        activeOption: types.facebook as Types,
     }
 
     handleRadioButtonState = (type: string) => {
@@ -37,36 +37,42 @@ class Form extends React.Component<Props> {
 
     render() {
 
-        const { submitFn } = this.props
+        const { submitFn } = this.props;
+        const { activeOption } = this.state
 
 
         return (
             <div className={styles.wrapper}>
-                <Title text={"Dodaj " + descriptions[this.state.activeOption]} />
+                <Title text={"Dodaj " + descriptions[activeOption]} />
 
                 <form autoComplete="off" className={styles.form} onSubmit={submitFn}>
                     <div className={styles.inputWrapper}>
-                        <RadioInput htmlFor={types.twitter} id={types.twitter}
-                            checked={this.state.activeOption === types.twitter}
-                            onChange={() => this.handleRadioButtonState(types.twitter)}
-                            label="Twitter" />
-
-                        <RadioInput htmlFor={types.note} id={types.note}
-                            checked={this.state.activeOption === types.note}
-                            onChange={() => this.handleRadioButtonState(types.note)}
-                            label="Notatka" />
+                        <RadioInput htmlFor={types.facebook} id={types.facebook}
+                            checked={activeOption === types.facebook}
+                            onChange={() => this.handleRadioButtonState(types.facebook)}
+                            label="Facebook" />
 
                         <RadioInput htmlFor={types.article} id={types.article}
-                            checked={this.state.activeOption === types.article}
+                            checked={activeOption === types.article}
                             onChange={() => this.handleRadioButtonState(types.article)}
                             label="Artykuł" />
+
+                        <RadioInput htmlFor={types.note} id={types.note}
+                            checked={activeOption === types.note}
+                            onChange={() => this.handleRadioButtonState(types.note)}
+                            label="Notatka" />
                     </div>
 
-                    <Input name="name" label="Nazwa rasy" maxLength={30} />
-                    <Input name="link" label="Link do grupy zkwp fci fb" />
-                    <Input name="image" label="Obrazek" />
-                    <Input name="description" label="Opis rasy" componentType="textarea" />
-                    <Button text="Dodaj nową rasę psa" />
+                    <Input name="name" label={activeOption === types.facebook ? "Nazwa" : "Tytuł"} maxLength={30} />
+
+
+                    {activeOption === types.facebook ? (<Input name="link" label="Link do grupy zkwp fci na fb" />) : null}
+
+                    {activeOption !== types.note ? (<Input name="image" label="Obrazek" />) : null}
+
+
+                    <Input name="description" label={activeOption === types.facebook ? "Opis" : "Treść"} componentType="textarea" />
+                    <Button text="Dodaj" />
                 </form>
             </div>
         )
